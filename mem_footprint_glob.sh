feae121ff8896ffafdbb9cf29e0bfdf8f3833a5e
Engin Kayraklioglu <ekayrakli@gmail.com>
2017-08-10 21:14:04 -0400

    Add global memplotting scripts

---
+++ mem_footprint_glob.sh
@@ -0,0 +1,12 @@
+versions=( 0 2 3cons 3cons_sd 3incons 3incons_sd )
+
+average () {
+  cat $1 | tail -n5 | head -n4 | tr -s " "  | cut -d" " -f3 > __tmp
+  awk {'a+=$1} END {print a/NR}' __tmp
+  rm __tmp
+}
+
+for v in "${versions[@]}"
+do
+  average $1/out/*.$v.*out >> tmp_memplot_dump
+done
