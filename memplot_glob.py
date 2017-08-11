224fab49842c361109db17ad03a79ee5fbcf1272
Engin Kayraklioglu <ekayrakli@gmail.com>
2017-08-10 22:15:00 -0400

    Adjust cosmetics of memplot

--- memplot_glob.py
+++ memplot_glob.py
@@ -5,62 +5,72 @@ import os
 from collections import namedtuple
 from collections import defaultdict
 
-benchmarks = [ "DGEMM" , "Transpose", "Sparse" ]
+benchmarks = [ "DGEMM" , "Transpose", "Sparse", "DGEMM", "Transpose" ]
 for b in benchmarks:
     os.system("./mem_footprint_glob.sh " + b)
 
 mem_dict = defaultdict(list)
 
-num_versions = 6
-vrange = np.arange(0,6)
-
 rects = []
-ind = np.arange(0, len(benchmarks))
-width = 0.1
+ind = np.arange(0, len(benchmarks)*1.2, 1.2)
+width = 0.125
 
-rect = 0.2,0.1,0.7,0.8
-fig = plt.figure(figsize=(7,10))
+rect = 0.1,0.12,0.85,0.8
+fig = plt.figure(figsize=(13,5))
 ax = fig.add_axes(rect)
 
-plt.tick_params(axis='x', which='both', bottom='off', top='off',
-        labelbottom='off')
-
+plt.tick_params(axis='x', which='both', bottom='off', top='off')
 
 VersionType = namedtuple("VersionType", "label color hatch")
 versions = [
     VersionType("Base", "#ca0020", ""),
     VersionType("HandOpt", "#f4a582", ""),
-    VersionType("AC", "#92c5de", "+"),
-    VersionType("MC", "#0571b0", "x"),
-    VersionType("AC-SD", "#92c5de", "+"),
-    VersionType("MC-SD", "#0571b0", "x")]
+    VersionType("AC", "#92c5de", ""),
+    VersionType("AC-SD", "#92c5de", "......"),
+    VersionType("MC", "#0571b0", ""),
+    VersionType("MC-SD", "#0571b0", "......")]
 
-max_mem = 0.
 with open("tmp_memplot_dump") as f:
   for b in benchmarks:
       for v in versions:
           mem = int(f.readline())
-          # mem = (mem[0] / 2**20, )
-          if mem > max_mem:
-              max_mem = mem
           mem_dict[v].append(mem)
 
 os.system("rm tmp_memplot_dump")
 
+# normalize footprints and find the max
+max_mem = 0.
+base_mem_list = mem_dict[versions[0]]
 for v in versions:
-    print(mem_dict[v])
-    rects.append(ax.bar(ind, mem_dict[v], width, edgecolor="k",
+    mem_dict[v] = [mem/base for mem,base in zip(mem_dict[v], base_mem_list)]
+    tmp_max = max(mem_dict[v])
+    if tmp_max > max_mem:
+        max_mem = tmp_max
+
+# plot bars
+count = 0
+pad = 0.04
+pad_cur = 0.
+for v in versions:
+    rects.append(ax.bar([i+count*width+pad_cur for i in ind],
+                     mem_dict[v], width, 
+                     linewidth=2, edgecolor="k",
                                    label=v.label,
                                    color=v.color,
                                    hatch=v.hatch))
-    ind = [i+width for i in ind]
+    count += 1
+    if count == 2:
+        pad_cur = pad
 
 
-ax.set_ylim((0, max_mem*1.25))
-ax.legend(framealpha=1, fontsize=18, ncol=2, loc='upper center')
+ax.set_ylim((0, max_mem*1.45))
+ax.legend(framealpha=1, fontsize=18, ncol=3, loc='upper center')
 ax.grid(b=True, axis='y', linestyle='dashed')
 ax.set_axisbelow(True)
-ax.set_ylabel("Memory Footprint (MB/Locale)")
+ax.set_ylabel("Normalized\nMemory Footprint")
+
+ax.set_xticks([i+(width*len(versions))/2-width/2+pad/2 for i in ind])
+ax.set_xticklabels(tuple(benchmarks))
 
 
 plt.show()
